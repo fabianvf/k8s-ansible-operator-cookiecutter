0383db4fb4513842e1990373a0b2debc2ed55189
Fabian von Feilitzsch <fabian@fabianism.us>
2018-12-17 15:32:50 -0500

    fix trap

--- {{cookiecutter.app_name}}/build/test-framework/ansible-test.sh
+++ {{cookiecutter.app_name}}/build/test-framework/ansible-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 (${OPERATOR:-/usr/local/bin/ansible-operator})&
-trap 'killall' SIGINT SIGTERM EXIT
+trap "kill $!" SIGINT SIGTERM EXIT
 
 cd ${HOME}/project
 exec "molecule test -s test-cluster"
